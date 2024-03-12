94ccc6ec5fd9ed248aa78f8c5a6a11ef7998c3bb
Caro <55109108+echocaro@users.noreply.github.com>
2024-03-11 20:30:15 -0500

    add workflow (#1)
    * add workflow
    
    * update go version
    
    * run for every commit
    
    * update yml
    
    * add another test

--- views/minutes_views_test.go
+++ views/minutes_views_test.go
@@ -30,13 +30,25 @@ func TestMinutesStartTimer(t *testing.T) {
 	interval := 1 * time.Second
 
 	model := views.NewMinutesModel(duration, interval)
-	minutesModel := model.(views.MinutesModel)
 
 	t.Run("Changes MinutesFirstRunComplete to true", func(t *testing.T) {
+		minutesModel := model.(views.MinutesModel)
 		minutesModel.StartTimer(duration, interval)
 
 		if minutesModel.MinutesFirstRunComplete != true {
 			t.Errorf("Expected MinutesFirstRunComplete to be true, got %v", minutesModel.MinutesFirstRunComplete)
+		}
+	})
+	t.Run("Resets timer if MinutesFirstRunComplete is true", func(t *testing.T) {
+		minutesModel := model.(views.MinutesModel)
+		minutesModel.MinutesFirstRunComplete = true
+
+		minutesModel.StartTimer(duration, interval)
+
+		// the Reset func should be triggered on the third run
+		// which means the id of the Timer should be 3
+		if minutesModel.Timer.ID() != 3 {
+			t.Errorf("Expected ID to be 3, got %v", minutesModel.Timer.ID())
 		}
 	})
 }
